f4395ddee585b7c5164b0889684c5d6d05ff7aa6
Alexey Sergey <alexey.sergey@softswiss.com>
2021-03-24 12:32:51 +0300

    fix ws location

--- backend/ws/ws_service.go
+++ backend/ws/ws_service.go
@@ -3,7 +3,6 @@ package ws
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net"
 	"net/http"
 
@@ -31,7 +30,7 @@ func (wsService *WsService) Serve() {
 	wsService.connections = make(map[uuid.UUID]net.Conn)
 
 	http.HandleFunc("/", wsService.Socket)
-	go log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", wsService.configure.Config.WebSocketPort), nil))
+	go log.Fatal(http.ListenAndServe(":9002", nil))
 }
 
 func (wsService *WsService) Stop() {
